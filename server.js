c06ffcde3760674b6a425320a3ba284774022f56
DESKTOP-BGOGHIS\BurkLesmeisterholder <joshburkholder31@gmail.com>
2017-08-07 01:24:42 -0400

    server

---
+++ server.js
@@ -0,0 +1,16 @@
+var http = require('http'),
+    url = require('url'),
+    server = http.createServer();
+
+
+server.on('request', function(req, res){
+        console.log("-- incoming request --", req.url);
+    var incomingUrl = url.parse(req.url);
+        console.log(incomingUrl);
+    // res.writeHead(200, {'Content-Type':'text/plain'});
+    // res.end('Hello World')
+    
+});
+
+server.listen(9000);
+
