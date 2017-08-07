f6dd0e1d5c8e464e7e388b61ebc59efecf705640
DESKTOP-BGOGHIS\BurkLesmeisterholder <joshburkholder31@gmail.com>
2017-08-06 23:58:10 -0400

    added command space

--- index.js
+++ index.js
@@ -4,7 +4,23 @@ var readline = require("readline"),
 rl.setPrompt("-->");
 rl.prompt();
 
+var toDoList = [];
+
 rl.on('line', function(line){
-    console.log(line);
+    var words = line.split(' ');
+    command = words.shift();
+    argStr = words.join(' ');
+
+    if(command === 'list'){
+        console.log(toDoList);
+    }
+    if(command === 'add'){
+        var item = words.join(' ');
+        toDoList.push(item)
+    }
+
+    // toDoList.push(line);
+    // console.log(toDoList);
+    rl.prompt();
 });
 
