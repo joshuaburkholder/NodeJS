47ce0c07a1f71b01eb45c9e09abd672d69ab1005
DESKTOP-BGOGHIS\BurkLesmeisterholder <joshburkholder31@gmail.com>
2017-08-07 01:08:06 -0400

    add remove

--- index.js
+++ index.js
@@ -6,21 +6,32 @@ rl.prompt();
 
 var toDoList = [];
 
+var commands = {
+    list: function(){
+        console.log(toDoList);
+    },
+    add: function(item){
+        toDoList.push(item);
+    },
+    remove: function(item){
+       console.log(item);
+        var index = toDoList.indexOf(item);
+        console.log(index);
+        if(index != -1)
+            toDoList.splice(index, 1);
+     }
+};
+
 rl.on('line', function(line){
     var words = line.split(' ');
-    command = words.shift();
-    argStr = words.join(' ');
+        command = words.shift();
+        argStr = words.join(' ');
 
-    if(command === 'list'){
-        console.log(toDoList);
-    }
-    if(command === 'add'){
-        var item = words.join(' ');
-        toDoList.push(item)
-    }
+    commands[command](argStr); 
 
-    // toDoList.push(line);
-    // console.log(toDoList);
     rl.prompt();
 });
 
+
+// var index = array.indexOf(item);
+// array.splice(index, 1);
