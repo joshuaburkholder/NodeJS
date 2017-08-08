51d668823a8615fae9b826f8ba5fa324908cce14
DESKTOP-BGOGHIS\BurkLesmeisterholder <joshburkholder31@gmail.com>
2017-08-08 01:28:14 -0400

    updated with pathname

--- mapping_req.js
+++ mapping_req.js
@@ -6,33 +6,30 @@ var path = require('path'),
 
 var server = http.createServer();
 
-//genericSend(404, "not found")
 function genericSend(code, message, res){
   res.writeHead(code, {'Content-Type':'text/plain'});
   res.end(message);
-}
+};
 
 server.on('request', function(req, res){
-    //404
-    //500
-    //200
 
   var urlParams = url.parse(req.url),
       //pathname: /index.html ==> index.html
       filename = path.join('.', urlParams.pathname)
 
-  path.exists(filename, function(exists){
+  fs.exists(filename, function(exists){
     if(!exists)
         return genericSend(404, 'not found');
 
     fs.readFile(filename, 'binary', function(err, file){
       if(err)
         return genericSend(500, 'internal server error'); 
+
       var type = mime.lookup(filename);
       res.writeHead(200, {'Content-Type':type});
       res.write(file,'binary');
       res.end();
-    })
+    });
   });
 });
 
