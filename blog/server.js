e34e0313502763c3e0a3efb9de3202fedbb07d9f
DESKTOP-BGOGHIS\BurkLesmeisterholder <joshburkholder31@gmail.com>
2017-08-09 01:32:56 -0400

    updated routes

--- blog/server.js
+++ blog/server.js
@@ -3,7 +3,14 @@ var express = require('express'),
 
 //CRUD 
 
+var notImplemented = function(req, res){
+    res.send(501)
+}
 
+app.get('/articles', notImplemented); //show all articles
+app.post('/articles', notImplemented); 
+app.put('/article/:articleId'notImplemented);
+app.del('/article/:articleId', notImplemented);
 
 
 app.listen(8080);
