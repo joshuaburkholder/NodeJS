54eda7c219d147dc929efbc4f4e3f14bb94d6f70
DESKTOP-BGOGHIS\BurkLesmeisterholder <joshburkholder31@gmail.com>
2017-08-09 01:36:06 -0400

    added comment routes

--- blog/server.js
+++ blog/server.js
@@ -3,14 +3,30 @@ var express = require('express'),
 
 //CRUD 
 
+/*
+a post will look like this:
+{
+    title: "",
+    body: "",
+    author: "",
+    comments: []
+}
+*/
+
+
 var notImplemented = function(req, res){
     res.send(501)
 }
 
 app.get('/articles', notImplemented); //show all articles
+app.get('/articles/:articleId', notImplemented);
+app.get('/articles/new', notImplemented);
 app.post('/articles', notImplemented); 
-app.put('/article/:articleId'notImplemented);
-app.del('/article/:articleId', notImplemented);
+app.put('/articles/:articleId'notImplemented);
+app.del('/articles/:articleId', notImplemented);
 
+//comments on blog
+app.post('articles/:articleId/comments', notImplemented);
+app.del('articles/:articleId/comments/:commentId', notImplemented);
 
 app.listen(8080);
