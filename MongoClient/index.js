478730ae1b09212020fba6a1133f900a14ce2a59
DESKTOP-BGOGHIS\BurkLesmeisterholder <joshburkholder31@gmail.com>
2017-08-11 01:40:26 -0400

    inserted doc into collection

--- MongoClient/index.js
+++ MongoClient/index.js
@@ -3,7 +3,13 @@
 //query that collection
 
 var MongoClient = require('mongodb').MongoClient;
-MongoClient.connect('mongodb://127.0.0.1:27017/example', function(err, db){
+MongoClient.connect('mongodb://127.0.0.1:27017/', function(err, db){
   if(err) return console.log(err);
-  console.log(db);
+  console.log("Connected to Mongo");
+//insert a doc into the collection
+  var collection = db.collection('users');
+  collection.insert({name:'Bob'}, function(err,docs){
+    if(err) return console.log(err);
+    console.log(docs);
+  });
 });
