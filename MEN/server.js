cf32c027cde81add3393278f2c70749bd02358ce
DESKTOP-BGOGHIS\BurkLesmeisterholder <joshburkholder31@gmail.com>
2017-08-11 02:00:27 -0400

    updated dep

---
+++ MEN/server.js
@@ -0,0 +1,16 @@
+var express = require('express'),
+    app = express(),
+    port = 8080,
+    MongoClient = require('mongodb').MongoClient,
+    bodyParser = require('body-parser'),
+    morgan = require('morgan');
+
+app.use(bodyParser());
+
+app.use(morgan());
+app.use(express.static(__dirname + '/public'));
+
+app.set('view engine', 'ejs');
+
+app.listen(port);
+console.log("Server is listening on %s", port);
