eef77534976ef25850c91ae0fa8d49a96daaaaa4
DESKTOP-BGOGHIS\BurkLesmeisterholder <joshburkholder31@gmail.com>
2017-08-12 00:54:05 -0400

    mapped controller

--- MongooseToDo/controllers/todo.js
+++ MongooseToDo/controllers/todo.js
@@ -3,15 +3,41 @@ var mongoose = require('mongoose'),
     controller = {};
 
 //collect & display all todo's
-
+controller.index = [
+  function(req, res, next){
+      //fn to collect & render view to display todo's
+  }
+];
 
 //create a todo
-
+controller.create = [
+  function(req, res, next){
+    //fn to validate input
+    next();
+  },
+  function(){
+    //fn to create teh todo, send json obj
+  }
+];
 
 //update a todo
-
+controller.update = [
+  function(req, res, next){
+    //load the todo to be updated
+    //validate input
+    next();
+  },
+  function(req, res, next){
+    //update todo, send back json obj
+  }
+];
 
 //delete a todo
+controller.delete = [
+  function(req, res, next){
+    //remove a todo - res 201
+  }
+];
 
-
-//export - 
+//export controller 
+module.exports = controller;
