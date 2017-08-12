350c032638d705a8da811e482bc6e892f3f2ffb2
DESKTOP-BGOGHIS\BurkLesmeisterholder <joshburkholder31@gmail.com>
2017-08-12 00:46:09 -0400

    update dependencies

--- MongooseToDo/server.js
+++ MongooseToDo/server.js
@@ -2,26 +2,23 @@ var express = require('express'),
     app = express(),
     bodyParser = require('body-parser'),
     mongoose = require('mongoose'),
+    morgan = require('morgan');
     // todoController = require('./controllers/todo');
 
 mongoose.connect('mongodb://localhost/mgoosetodo')
 
 app.use(bodyParser.json());
-app.use(express.urlencoded());
+app.use(bodyParser.urlencoded({extended: true}));
 app.use(morgan('combined'));
 
 app.set('view engine', 'ejs');
 
 // routes
 
-app.get();
-app.post();
-app.put();
-app.delete();
-
-app.listen(port, function(err){
-  console.log('server on %s', port);
-});
+// app.get();
+// app.post();
+// app.put();
+// app.delete();
 
 app.listen(8080, function(){           
   console.log("Server is listening on 8080");  
