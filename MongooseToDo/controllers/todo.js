74318e484b2f3e1002a70353ea2fe7a1e32c3ce9
DESKTOP-BGOGHIS\BurkLesmeisterholder <joshburkholder31@gmail.com>
2017-08-12 00:49:16 -0400

    added todo controller

---
+++ MongooseToDo/controllers/todo.js
@@ -0,0 +1,17 @@
+var mongoose = require('mongoose'),
+    Todo = mongoose.model('todo'),
+    controller = {};
+
+//collect & display all todo's
+
+
+//create a todo
+
+
+//update a todo
+
+
+//delete a todo
+
+
+//export - 
