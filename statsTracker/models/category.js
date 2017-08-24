f925731f38bc390e8ef62fa4f20d7447fc9aa81b
DESKTOP-BGOGHIS\BurkLesmeisterholder <joshburkholder31@gmail.com>
2017-08-24 01:13:39 -0400

    update category

---
+++ statsTracker/models/category.js
@@ -0,0 +1,22 @@
+const mongoose = require('mongoose');
+const uniqueValidator = require('mongoose-unique-validator');
+
+let Schema = mongoose.Schema;
+
+const categorySchema = new mongoose.Schema({
+
+  activity_type: {
+    type: String,
+  },
+  stats: [{
+    type: String,
+    ref: 'Activity',
+  }],
+});
+
+categorySchema.plugin(uniqueValidator);
+
+const Category = mongoose.model('Category', categorySchema);
+
+
+module.exports = Category
