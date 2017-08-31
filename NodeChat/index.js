89b259b283a7d709b6240a94dfc78ecd9e85be2d
DESKTOP-BGOGHIS\BurkLesmeisterholder <joshburkholder31@gmail.com>
2017-08-30 23:54:42 -0400

    update

--- NodeChat/index.js
+++ NodeChat/index.js
@@ -4,10 +4,6 @@ var jade = require('jade');
 var app = express();
 var port = 3700;
 
-app.get('/', function(req, res){
-	res.send("tada!");
-});
-
 app.set('views', __dirname + '/tpl');
 app.set('view engine', 'jade');
 app.engine('jade', require('jade').__express);
