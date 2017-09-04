f5e90ca254a90ff1baac979a1a6adce9dee92266
DESKTOP-BGOGHIS\BurkLesmeisterholder <joshburkholder31@gmail.com>
2017-09-03 23:54:32 -0400

    create router instance

--- Express4/server.js
+++ Express4/server.js
@@ -9,13 +9,21 @@ app.get('/sample', function(req, res){
   res.send('Sample app');
 });
 
+// Create an instance of the Express router
+
+var router = express.Router();
+
+// Route to HOME 
+
+// Route to ABOUT
+
+// Apply routes
 
 
 
 
 
-
-//SERVER START
+// SERVER START
 
 app.listen(port);
 console.log('Server listening on port ' + port);
