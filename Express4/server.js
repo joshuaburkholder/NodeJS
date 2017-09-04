e751af3c035792341327e36e3cbb77d55792c0f4
DESKTOP-BGOGHIS\BurkLesmeisterholder <joshburkholder31@gmail.com>
2017-09-03 23:50:39 -0400

    server init

---
+++ Express4/server.js
@@ -0,0 +1,17 @@
+var express = require('express');
+var app = express();
+var port = process.env.PORT || 8080;
+
+
+//ROUTES
+
+
+
+
+
+
+
+//SERVER START
+
+app.listen(port);
+console.log('Server listening on port ' + port);
