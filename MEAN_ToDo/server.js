b1311f704880bdde132ad8f266fa6627c6d5a184
DESKTOP-BGOGHIS\BurkLesmeisterholder <joshburkholder31@gmail.com>
2017-09-04 00:40:28 -0400

    update server

--- MEAN_ToDo/server.js
+++ MEAN_ToDo/server.js
@@ -7,3 +7,13 @@ var bodyParser = require('body-parser');
 var methodOverride = require('method-override');
 
 mongoose.connect('mongodb://localhost/meantodo');
+
+app.use(express.static(__dirname + '/public'));
+app.use(morgan('dev'));
+app.use(bodyParser.urlencoded({ 'extended': 'true' }));
+app.use(bodyParser.json());
+app.use(bodyParser.json({ type: 'application/vnd.api+json' }));
+app.use(methodOverride());
+
+app.listen(8080);
+console.log('Server listening on port 8080');
