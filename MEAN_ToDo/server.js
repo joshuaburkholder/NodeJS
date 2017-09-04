16dedb8b0497ea8de792cd93e8a80a2259f86c2f
DESKTOP-BGOGHIS\BurkLesmeisterholder <joshburkholder31@gmail.com>
2017-09-04 00:37:09 -0400

    initial server

---
+++ MEAN_ToDo/server.js
@@ -0,0 +1,9 @@
+var express = require('express');
+var app = express();
+
+var mongoose = require('mongoose');
+var morgan = require('morgan');
+var bodyParser = require('body-parser');
+var methodOverride = require('method-override');
+
+mongoose.connect('mongodb://localhost/meantodo');
